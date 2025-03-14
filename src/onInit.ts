a9555e5cacb9d0c2e07b28f9bc2631b8002296aa
lrp-br <louise.pedersen@br-automation.com>
2025-03-14 14:23:50 +0100

    background changes based on data inputinput

--- src/onInit.ts
+++ src/onInit.ts
@@ -3,26 +3,33 @@ import * as THREE from "three";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const webglCanvas: HTMLElement = htmlNode.getElementById('webglCanvas')!;
-export let renderer: THREE.WebGLRenderer;
-checkWebGLCanvasUsage(webglCanvas);
+//Make canvas and renderer outside of function so it can be exported
+export const canvas = htmlNode.getElementById('webglCanvas') as HTMLCanvasElement;
+export const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
+//setup of other veriables for this file
+let scene: THREE.Scene;
+let camera: THREE.PerspectiveCamera;
+let controls: OrbitControls;
 
-function checkWebGLCanvasUsage(htmlElement: HTMLElement) {
-    if (webglCanvas?.getAttribute('data-inUse') === 'true') {
+//Call main function of onRender
+checkWebGLCanvasUsage(canvas);
+
+function checkWebGLCanvasUsage(canvas: HTMLCanvasElement) {
+    // Check if canvas is already in use
+    if (canvas?.getAttribute('data-inUse') === 'true') {
         console.log("onInit code skipped")
         return; // Skip the rest of the code
     }
 
     //set scene and camera
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     //initialize renderer through canvas element
-    const renderer = new THREE.WebGLRenderer({ canvas: htmlElement as HTMLCanvasElement, alpha: true });
-    webglCanvas?.setAttribute('data-inUse','true');
+    canvas?.setAttribute('data-inUse', 'true');
     renderer.setSize(window.innerWidth - 50, window.innerHeight - 50);
     renderer.setClearColor(0xaaaaaa); // Light gray background
-    // htmlNode.appendChild(renderer.domElement);
+    htmlNode.appendChild(renderer.domElement);
 
     // Add light sources
     const ambientLight = new THREE.AmbientLight(0xffffff, 1); // Soft white light
@@ -33,7 +40,7 @@ function checkWebGLCanvasUsage(htmlElement: HTMLElement) {
     scene.add(directionalLight);
 
     // Set up OrbitControls
-    const controls = new OrbitControls(camera, renderer.domElement);
+    controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true; // Enable damping (inertia)
     controls.dampingFactor = 0.25; // Damping factor
     controls.screenSpacePanning = false; // Disable panning
@@ -48,18 +55,19 @@ function checkWebGLCanvasUsage(htmlElement: HTMLElement) {
         // Set the camera position and look at the scene
         camera.position.z = 5;
         camera.lookAt(scene.position);
-
-        // Render loop
-        function animate(): void {
-            requestAnimationFrame(animate);
-            controls.update(); // Update controls
-            renderer.render(scene, camera);
-        }
         animate();
+
     }, undefined, function (error) {
-        console.error('An error happened', error);
+        console.error('An error happened when loading the glb file and ', error);
     });
     console.log("one onInit function pass")
+}
+
+// Render loop
+function animate(): void {
+    requestAnimationFrame(animate);
+    controls.update(); // Update controls
+    renderer.render(scene, camera);
 }
 
 console.log("bottom of onInit reached");
--- src/onRender.ts
+++ src/onRender.ts
@@ -1,44 +1,102 @@
-import {renderer} from "./onInit";
+import { renderer } from "./onInit";
 
 console.log('onRender start')
 
-// get state string from db
-const valueField = data.series[0].fields[1];
-const arrayLength = valueField.values.length;
-const value = valueField.values.get(arrayLength - 1);
+render(); // call main function of onRender
+function render(): void {
+    // get state string from db
+    const valueField = data.series[0].fields[1];
+    const arrayLength = valueField.values.length;
+    const value = valueField.values.get(arrayLength - 1);
 
+    //check if value exists before continuing
+    if (!value) {
+        console.log('No new value', value)
+        return;
+    }
 
-if(value) {
-  switch(value) {
-    case 'Stopped': 
-        renderer.setClearColor(0xE44C5C);
-        break;
-    case 'Resetting':
-        renderer.setClearColor(0x267EB6);
-        break;
-    case 'Idle':
-        renderer.setClearColor(0x3BB626);
-        break;
-    case 'Starting': 
-        renderer.setClearColor(0xB62E26);
-        break;
-    case 'Execute':
-        renderer.setClearColor(0x6B4CE4);
-        break;
-    case 'Aborting':
-        renderer.setClearColor(0x51E44C);
-        break;
-    case 'Aborted':
-        renderer.setClearColor(0x4C60E4);
-        break;
-    case 'Clearing':
-        renderer.setClearColor(0x4CC8E4);
-        break;
-    case 'Stopping':
-        renderer.setClearColor(0x4CE4CA);
-        break;
-  }
-  console.log(value);
+    //change background color based on state
+    if (value) {
+        switch (value) {
+            case 'Stopped':
+                renderer?.setClearColor(0xE44C5C);
+                break;
+            case 'Resetting':
+                renderer?.setClearColor(0x267EB6);
+                break;
+            case 'Idle':
+                renderer?.setClearColor(0x3BB626);
+                break;
+            case 'Starting':
+                renderer?.setClearColor(0xB62E26);
+                break;
+            case 'Execute':
+                renderer?.setClearColor(0x6B4CE4);
+                break;
+            case 'Aborting':
+                renderer?.setClearColor(0x51E44C);
+                break;
+            case 'Aborted':
+                renderer?.setClearColor(0x4C60E4);
+                break;
+            case 'Clearing':
+                renderer?.setClearColor(0x4CC8E4);
+                break;
+            case 'Stopping':
+                renderer?.setClearColor(0x4CE4CA);
+                break;
+        }
+        console.log(value);
+    } 
+    console.log('reached bottom of onRender');
 }
 
-console.log('rendering complete');
+
+
+ /* CODE FOR USE WHEN ONLY USING LOCALHOST VIA "npm run dev"
+ *------------- Sets value as randomized state -------------*/
+
+// setInterval(function setRandomState(): void {
+//     const states = ["Stopped", "Resetting", "Idle", "Starting", "Execute", "Aborting", "Aborted", "Clearing", "Stopping"];
+
+//     const random = Math.floor(Math.random() * states.length);
+//     let value = states[random];
+//     console.log(random, states[random]);
+
+
+//     if (value) {
+//         switch (value) {
+//             case 'Stopped':
+//                 renderer.setClearColor(0xE44C5C);
+//                 break;
+//             case 'Resetting':
+//                 renderer.setClearColor(0x267EB6);
+//                 break;
+//             case 'Idle':
+//                 renderer.setClearColor(0x3BB626);
+//                 break;
+//             case 'Starting':
+//                 renderer.setClearColor(0xB62E26);
+//                 break;
+//             case 'Execute':
+//                 renderer.setClearColor(0x6B4CE4);
+//                 break;
+//             case 'Aborting':
+//                 renderer.setClearColor(0x51E44C);
+//                 break;
+//             case 'Aborted':
+//                 renderer.setClearColor(0x4C60E4);
+//                 break;
+//             case 'Clearing':
+//                 renderer.setClearColor(0x4CC8E4);
+//                 break;
+//             case 'Stopping':
+//                 renderer.setClearColor(0x4CE4CA);
+//                 break;
+//         }
+//         console.log(value);
+
+//     }
+
+//     console.log('rendering complete');
+// }, 5000)
